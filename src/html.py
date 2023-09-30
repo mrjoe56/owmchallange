25ab27ec6444ef0416545e9436dacc5ff511342d
joe@respirationscan.com <joe@respirationscan.com>
2023-09-30 17:29:35 +0100

    some error handling

--- src/html.py
+++ src/html.py
@@ -28,22 +28,20 @@ def form_post(request: Request, zip: str = Form(...)):
     zip_instance = ZIPCODE(zip)
     response = zip_instance.result()
 
-    #print(response)
+    print(response)
 
-    if 'cod' in response:
-        if response['cod'] == '404':
-            result = response['message']
-            return templates.TemplateResponse('error.html', context={'request': request, 'direction': DIRECTION, 'result': result, 'zip': zip})
+    if 'cod' in response and response['cod'] == '404':
+        result = response['message']
+        return templates.TemplateResponse('error.html', context={'request': request, 'direction': DIRECTION, 'result': result, 'zip': zip})
     else:
         forecast_instance = FORECAST(response['lat'], response['lon'])
         response = forecast_instance.result()
-        if 'cod' in response:
-            if response['cod'] == '404':
-                result = response['message']
-                return templates.TemplateResponse('error.html', context={'request': request, 'direction': DIRECTION, 'result': result, 'zip': zip})
-        else:
-            print("this is the response after providing lan, lon data", response)
-            parser_instance = PARSER(response)
-            result = parser_instance.datalist()
+        if 'cod' in response and response['cod'] == '404':
+            result = response['message']
+            return templates.TemplateResponse('error.html', context={'request': request, 'direction': DIRECTION, 'result': result, 'zip': zip})
+
+        print("this is the response after providing lan, lon data", response)
+        parser_instance = PARSER(response)
+        result = parser_instance.datalist()
     
     return templates.TemplateResponse('form.html', context={'request': request, 'direction': DIRECTION, 'result': result, 'zip': zip})
