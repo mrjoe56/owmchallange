75ec32873e4c9cefd06942bf025ff91ae5a8016f
joe@respirationscan.com <joe@respirationscan.com>
2023-10-01 14:34:29 +0100

    removed repeations

--- src/html.py
+++ src/html.py
@@ -4,12 +4,9 @@ import pycountry
 from wrapper.zipcode import Zipcode
 from wrapper.forecast import Forecast
 from wrapper.parser import Parser
-from wrapper.day import Day
 
-from fastapi import FastAPI, HTTPException, status, Request, Response, Form
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
-from starlette.responses import FileResponse
-from starlette.background import BackgroundTask
 
 DIRECTION = [
         "Please enter the requested Zip code",
@@ -17,6 +14,10 @@ DIRECTION = [
         "By default, the zipcode entered checks against US zipcodes",
         "If you need a specific country's 5 days weather forecast, use: zip,XX format (i.e. 10004,US)",
         "Or, just use the country drop-down option",
+        "",
+        "Note: openweathermap zip api does not support all countries",
+        "Tested countries that openweathermap zip API does return lan,lot for: Great Britain, Unites States, Germany, Italy, Spain, France, Sweeden, Finland",
+        "Tested countries that openweathermap zip API does not return lat,lon for: Brazil, Israel",
         ""
         ]
 
@@ -28,7 +29,6 @@ templates = Jinja2Templates(directory='templates/')
 
 country_list = [i.alpha_2 for i in pycountry.countries]
 country_list_of_dict = [{'name': i.name, 'code': i.alpha_2} for i in pycountry.countries]
-print(country_list_of_dict)
 country_list.sort()
 
 @app.get('/')
@@ -47,16 +47,16 @@ def form_post(request: Request, zip: str = Form(None), selected: bool = False, c
     response = zip_instance.result()
 
     if 'cod' in response: # cheking for response code (mainly for errors)
-        result = response['message']
+        result = {'error': 'yes', 'code': response['cod'], 'message': response['message']}
 
-        return templates.TemplateResponse(FORM, context={'request': request, 'direction': DIRECTION, 'countries': country_list_of_dict, 'selected': selected,  'result': result, 'zip': zip})
+        #return templates.TemplateResponse(FORM, context={'request': request, 'direction': DIRECTION, 'countries': country_list_of_dict, 'selected': selected,  'result': result, 'zip': zip})
     else:
         forecast_instance = Forecast(response['lat'], response['lon'])
         response = forecast_instance.result()
-        if 'cod' in response and response['cod'] == '404': # cheking for response code (mainly for errors)
-            result = response['message']
+        if 'cod' in response: # cheking for response code (mainly for errors)
+            result = {'error': 'yes', 'code': response['cod'], 'message': response['message']}
 
-            return templates.TemplateResponse(FORM, context={'request': request, 'direction': DIRECTION, 'countries': country_list_of_dict, 'selected': selected,  'result': result, 'zip': zip})
+            #return templates.TemplateResponse(FORM, context={'request': request, 'direction': DIRECTION, 'countries': country_list_of_dict, 'selected': selected,  'result': result, 'zip': zip})
 
         parser_instance = Parser(response)
         result = parser_instance.datadict()
